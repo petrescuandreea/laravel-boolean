09a87d133cd1efd7450d68f990a6c8662c2881af
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-15 16:43:28 +0100

    Add postcard.create and postcard.store in web

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'GuestController@home') -> name('home');
 
+Route::get('/postcard/create', 'GuestController@createPostcard') -> name('postcard.create');
+Route::post('/postcard/store', 'GuestController@storePostcard') -> name('postcard.store');
+
 Route::get('/api/postcards/index', 'ApiController@getPostcards') -> name('api.postcards.index');
