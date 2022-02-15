827b6ecf9674cf6cd0a3d3361f0b99015eeca4a9
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-15 17:03:31 +0100

    Add register, login and logout routes in web

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'GuestController@home') -> name('home');
 
+Route::post('/register', 'Auth\RegisterController@register') -> name('register');
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
+
 Route::get('/postcard/create', 'GuestController@createPostcard') -> name('postcard.create');
 Route::post('/postcard/store', 'GuestController@storePostcard') -> name('postcard.store');
 
