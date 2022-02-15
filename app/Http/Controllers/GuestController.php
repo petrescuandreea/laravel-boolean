6712b7f3b8b6afba4ef3acf6a196ebc9c82c1641
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-15 16:44:16 +0100

    Add createPostcard and StorePostcard methods in GuestController

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -4,10 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Postcard;
+
 class GuestController extends Controller
 {
     public function home() {
 
         return view('pages.home');
+    }
+
+    public function createPostcard() {
+
+        return view('pages.create-postcard');
+    }
+
+    public function storePostcard(Request $request) {
+
+        // validazione dati 
+        $data = $request -> validate([
+
+            'sender' => 'required|string',
+            'address' => 'required|string',
+            'text' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg',
+        ]);
+        
+        // salvo il nome dell'immagine da storePostcard in una variabile
+        $imageFile = $data['image'];
+        
+        // genero un nome casuale per l'immagine => rand()
+        // aggiungo l'estensione originale getClientOriginalExtension()
+        $imageName = rand(100000, 999999) . '_' . time()
+                    . '.' . $imageFile -> getClientOriginalExtension();
+        
+        // sposto il nome dell'immagine nella cartella postcards
+        $imageFile -> storeAs('/postcards', $imageName, 'public');
+
+        // aggiorno valore image 
+        $data['image'] = $imageName;
+
+        // creo la nuova postcard 
+        $postcard = Postcard::create($data);
+
+        return redirect() -> route('home');
     }
 }
