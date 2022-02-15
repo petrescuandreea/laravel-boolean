ddb4c80f1242abaec0781d046ac49fd7fa12f452
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-15 17:04:07 +0100

    Add register and login form in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,57 @@
 @extends('layouts.main-layout')
 @section('content')
-    <a class="btn btn-primary" href="{{ route('postcard.create') }}">ADD POSTCARD</a>
+
+    @auth
+        <h1>
+            Hello {{ Auth::user() -> name }}
+        </h1>
+
+        <h2>
+            Feel free to add one!!
+        </h2>
+
+        <a class="btn btn-danger" href="{{ route('logout') }}">LOGOUT</a>
+        <a class="btn btn-primary" href="{{ route('postcard.create') }}">ADD POSTCARD</a>
+
+    @else
+        {{-- login form  --}}
+        <h2>Login</h2>
+        <form action="{{ route('login') }}" method="POST">
+            @method("POST")
+            @csrf
+
+            <label for="email">Email</label><br>
+            <input type="email" name="email"><br>
+
+            <label for="password">Password</label><br>
+            <input type="password" name="password"><br>
+            <br>
+
+            <button class="btn btn-primary" type="submit" > Login</button>
+        </form>
+
+        {{-- registration form  --}}
+        <h2>Registration</h2>
+        <form action="{{ route('register') }}" method="POST">
+            @method("POST")
+            @csrf
+
+            <label for="name">Name</label><br>
+            <input type="text" name="name"><br>
+
+            <label for="email">Email</label><br>
+            <input type="email" name="email"><br>
+
+            <label for="password">Password</label><br>
+            <input type="password" name="password"><br>
+
+            <label for="password_confirmation">Password Confirmation</label><br>
+            <input type="password" name="password_confirmation"><br>
+            <br>
+
+            <button class="btn btn-primary" type="submit" > Register</button>
+        </form>
+    @endauth
 
     <postcards-component></postcards-component>
 @endsection
