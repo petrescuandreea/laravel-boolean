9cfe934537177562314cadd308e7bd4e5ad16c9d
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-15 12:59:28 +0100

    Create Postcards component

---
+++ resources/js/components/PostcardsComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div v-for="postcard,i in postcards" :key="i" class="card">
+                    <div class="card-header">Postcard sended by <b> {{ postcard.sender }} </b> from {{ postcard.address }} </div>
+
+                    <div class="card-body">
+                        <p>
+                            {{ postcard.text}}
+                        </p>
+                        <p>
+                            {{postcard.image}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+
+            return{
+
+                postcards: [],
+            }
+        },
+        mounted() {
+            
+            axios.get('/api/postcards/index')
+                 .then(r => this.postcards = r.data)
+                 .catch(e => console.error('e', e));
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .card {
+        margin: 20px 0;
+    }
+</style>
