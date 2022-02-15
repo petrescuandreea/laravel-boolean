a7e470e4939aff2e1d933e0b02f32044f39090cc
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-15 16:42:55 +0100

    Add button in home page for new postcard

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.main-layout')
 @section('content')
+    <a class="btn btn-primary" href="{{ route('postcard.create') }}">ADD POSTCARD</a>
+
     <postcards-component></postcards-component>
 @endsection
