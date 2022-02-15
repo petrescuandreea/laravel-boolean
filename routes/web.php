7c40fffd88d38d6be8d1d6b4b560ecab2968393a
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-15 12:59:57 +0100

    Add api.postcards.index route in web

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'GuestController@home') -> name('home');
+
+Route::get('/api/postcards/index', 'ApiController@getPostcards') -> name('api.postcards.index');
